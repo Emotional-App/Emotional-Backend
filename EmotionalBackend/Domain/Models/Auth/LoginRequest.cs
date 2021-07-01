f1b0de787e7a9dd82c51388f753b4c7d9909a7d1
teinnt <phuongthao234567@gmail.com>
2021-07-01 19:59:58 +1200

    setup jwt and Auth middleware

---
+++ EmotionalBackend/Domain/Models/Auth/LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emotional.Api.Domain.Models.Auth
+{
+    public class LoginRequest
+    {
+        public string UserName { set; get; }
+        public string Password { set; get; }
+        public string RememberMe { set; get; }
+    }
+}
